0bdda018ccd1eb7db7e960be76e357bf90963bca
Matt Hazelwood <113630447+matt-hazel@users.noreply.github.com>
2026-06-25 01:51:41 -0500

    Testing with defending

--- bot/actions.py
+++ bot/actions.py
@@ -1,10 +1,61 @@
+import threading
+import time
+
 from javascript import require
 
 pathfinder = require('mineflayer-pathfinder')
+
+max_distance = 1
 
 
 def walk_to(bot, x, y, z):
     movements = pathfinder.Movements(bot)
     bot.pathfinder.setMovements(movements)
+    movements.allowParkour = True
+    movements.allowSprinting = True
+    movements.allowJump = True
+    movements.allowClimb = True
+    movements.allowSwim = True
+    movements.allowFall = True
+    movements.allowOpenDoors = True
+    movements.allowBreakBlocks = True
+    movements.allowPlaceBlocks = True
+    movements.allowUseItems = True
+    movements.canScaffold = True
     goal = pathfinder.goals.GoalNear(x, y, z, 1)
     bot.pathfinder.setGoal(goal)
+    
+def nearest_entity(bot, entity_type):
+    closest = None
+    closest_dist = max_distance
+    for key in bot.entities:
+        try:
+            entity = bot.entities[key]
+            if entity is None:
+                continue
+            #print(f"Entity: {entity.name}, Type: {entity.type}")
+            if entity.type != 'mob':
+                continue
+            if entity.type != 'hostile':
+                continue
+            dist = bot.entity.position.distanceTo(entity.position)
+            if dist < closest_dist:
+                closest = entity
+                closest_dist = dist
+        except:
+            continue
+            
+    return closest
+
+def defend(bot):
+    target = nearest_entity(bot, 'mob')
+    if target:
+        bot.attack(target)
+    
+def start_threat_monitoring(bot):
+    def monitor():
+        while True:
+            defend(bot)     
+            time.sleep(2)       
+    thread = threading.Thread(target=monitor, daemon=True)
+    thread.start()
--- bot/core.py
+++ bot/core.py
@@ -2,16 +2,12 @@ from time import time
 import sys
 from javascript import require, On, Once
 from actions import walk_to
+from events import register_events
 
 
 mineflayer = require('mineflayer')
 pathfinder = require('mineflayer-pathfinder')
 process = require('process')
-
-death_counter = 0
-kill_counter = 0
-block_mine_counter = 0
-block_place_counter = 0
 
 options = {
     'host': sys.argv[1],
@@ -24,43 +20,4 @@ options = {
 bot = mineflayer.createBot(options)
 bot.loadPlugin(pathfinder.pathfinder)
 
-@Once(bot, 'spawn')
-def on_spawn(this, *args):
-    bot.chat("Hola brochachos")
-    
-@On(bot, 'chat')
-def on_chat(this, username, message, *rest):
-    if username == bot.username:
-        return #ignore self
-    print(f"{username}: {message}")
-    if message == 'come':
-        pos = bot.players[username].entity.position
-        walk_to(bot, pos.x, pos.y, pos.z)
-    
-@On(bot, 'death')
-def on_death(*args):
-    global death_counter
-    death_counter += 1
-    bot.chat("darn it")
-    print("Bot died")
-    print(f"Death count: {death_counter}")
-    bot._client.write('client_command', {'payload': 0})
-    
-@On(bot, 'health')
-def on_health(this, *args):
-    print(f"Bot health: {bot.health}, Food: {bot.food}")
-    
-@On(bot, 'entityDead')
-def on_kill(this, *args):
-    global kill_counter
-    kill_counter += 1
-    print(f"Kill count: {kill_counter}")
-    
-@On(bot, 'diggingCompleted')
-def on_block_mine(this, *args):
-    global block_mine_counter
-    block_mine_counter += 1
-    print(f"Block mine count: {block_mine_counter}")
-
-
-#walk_to(bot, 44, 72, 39)
+register_events(bot)
