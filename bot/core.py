84493fc7ab14e252e7cedfefa18a5e5f98e1f47b
Matt Hazelwood <113630447+matt-hazel@users.noreply.github.com>
2026-06-25 00:31:03 -0500

    Fixed Username and Respawning issue

--- bot/core.py
+++ bot/core.py
@@ -1,4 +1,5 @@
-from concurrent.futures import process
+from time import time
+import sys
 from javascript import require, On, Once
 
 mineflayer = require('mineflayer')
@@ -11,10 +12,11 @@ block_mine_counter = 0
 block_place_counter = 0
 
 options = {
-    'host': process.argv[2],
-    'port': process.argv[3],
-    'username': process.argv[4],
-    'password': process.argv[5]
+    'host': sys.argv[1],
+    'port': sys.argv[2],
+    'username': sys.argv[3],
+    #'password': process.argv[4],
+    #'version': '1.21.1'
 }
 
 bot = mineflayer.createBot(options)
@@ -30,18 +32,19 @@ def on_chat(this, username, message, *rest):
     print(f"{username}: {message}")
     
 @On(bot, 'death')
-def on_death(this, *args):
+def on_death(*args):
     global death_counter
     death_counter += 1
     bot.chat("darn it")
     print("Bot died")
     print(f"Death count: {death_counter}")
+    bot._client.write('client_command', {'payload': 0})
     
 @On(bot, 'health')
 def on_health(this, *args):
     print(f"Bot health: {bot.health}, Food: {bot.food}")
     
-@On(bot, 'enityDead')
+@On(bot, 'entityDead')
 def on_kill(this, *args):
     global kill_counter
     kill_counter += 1
